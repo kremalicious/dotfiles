623860b8ac4995a1999c5d7251ddc3ef089ace39
Matthias Kretschmann <m@kretschmann.io>
2013-10-27 13:08:40 +0100

    TextMate 2 properties file

--- make.sh
+++ make.sh
@@ -14,7 +14,7 @@
 # dotfiles directory
 dir=~/.dotfiles
 # list of files/folders to symlink in homedir
-files="aliases bashrc bash_profile bash_prompt gitconfig gitignore_global hushlogin private"
+files="aliases bashrc bash_profile bash_prompt gitconfig gitignore_global hushlogin private tm_properties"
 
 # ----------------------------------------------------------------------
 # change to the dotfiles directory

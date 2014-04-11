c9ae684025bf74683593053aac541b378e288427
Matthias Kretschmann <m@kretschmann.io>
2014-04-11 15:55:20 +0200

    generator-assets config file

--- update.sh
+++ update.sh
@@ -14,7 +14,7 @@
 # dotfiles directory
 dir=~/Projects/dotfiles
 # list of files/folders to symlink in homedir
-files="aliases bashrc bash_profile bash_prompt gemrc gitconfig gitignore_global hushlogin private tm_properties"
+files="aliases bashrc bash_profile bash_prompt gemrc gitconfig gitignore_global hushlogin private tm_properties generator.json"
 
 # ----------------------------------------------------------------------
 # change to the dotfiles directory

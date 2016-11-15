f7218f1511c60fafad14a21ebe4e228e96904368
Matthias Kretschmann <m@kretschmann.io>
2016-11-15 22:28:53 +0100

    tweak cloud sync, backup S3

--- bin/sync-cloud.sh
+++ bin/sync-cloud.sh
@@ -1,17 +1,27 @@
 #!/usr/bin/env bash
-
+#
 #
 # Prerequisite:
-# rclone configured with remote Amazon Cloud Drive named `amazon`
+# rclone configured with:
+# - remote Amazon Cloud Drive named `amazon`
+# - remote AWS S3 named `s3`
 #
 # brew install rclone
 # rclone config
-
+#
 # http://rclone.org/commands/rclone_copy/
 # http://rclone.org/commands/rclone_sync/
 
-# Sync Movies
-rclone sync $MOVIES_DIR amazon:/Movies --update
+DEFAULTS_AMAZON='--exclude .DS_Store --transfers 2'
 
-# Sync TV Shows
-rclone sync $TVSHOWS_DIR amazon:/TV\ Shows --update
+# Sync up Movies
+rclone sync $MOVIES_DIR amazon:/Movies $DEFAULTS_AMAZON
+
+# Sync up TV Shows
+rclone sync $TVSHOWS_DIR amazon:"/TV Shows" $DEFAULTS_AMAZON
+
+# Sync up iTunes
+rclone sync $ITUNES_DIR amazon:/Music/iTunes $DEFAULTS_AMAZON
+
+# Sync down all S3 buckets
+rclone sync s3: $S3BACKUP_DIR
